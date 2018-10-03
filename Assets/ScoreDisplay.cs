21ef6eed92d18af3dffafcb1197d57c50a1e6395
yuukimatsuoka <yuuki-matsuoka@ae.auone-net.jp>
2018-10-03 17:17:53 +0900

    Lesson5 課題提出
    発展課題にチャレンジしたもののUnityRemoteが上手く動作しなかったため動作検証できず

---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    ScoreManager scoreManager;
+
+    //オブジェクトに触れたか判定する値
+    private int touchFlg = 0;
+
+    //得点
+    private int point = 0;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); 
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        //タグによって得点を設定
+        if (tag == "SmallStarTag")
+        {
+            this.point = 5;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.point = 30;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.point = 20;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            this.point = 50;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.touchFlg > 0)
+        {
+            scoreManager.score += this.point;
+
+            this.touchFlg = 0;
+        }
+
+        //ScoreTextにスコアを表示
+        this.scoreText.GetComponent<Text>().text = "Score:    " + scoreManager.score;
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        this.touchFlg = 1;
+        Debug.Log("aaaa");
+    }
+
+}
+
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    //スコアを表示するための変数を宣言
+    public int score = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
